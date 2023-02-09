1696c626016a284735c375d870f8152e986589bc
Katada <katada.alrawi@dci.education>
2023-02-09 19:08:56 +0100

    final

---
+++ schleife/index.js
@@ -0,0 +1,39 @@
+let i = 0;
+while (i < 15){
+    i++
+    console.log(i)
+}
+console.log("--------------")
+let h = 0;
+while (h <= 20){
+    h = h+ 1
+    console.log(h)
+}
+console.log("--------------")
+
+let K = 0;
+do {
+    console.log(`The number is ${i}`);
+    i= i+ 1
+  } while (i < 20);
+  console.log("-------finshed-------")
+
+
+  let katada = 0;
+  while (katada < 50){
+      console.log(katada)
+    katada++
+  }
+
+  let ginwa = 100;
+  do{
+    console.log(`my love ${ginwa }% is ghinwa`)
+    ginwa++
+
+  }while (ginwa < 50)
+
+let ayla = 20;
+do{
+    console.log(`my lovely girl ${ayla}`)
+    ayla++
+}while (ayla < 10)
